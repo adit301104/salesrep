810994879e22c3a0b61450faf06dcd591195ed83
Aditya Kumar <124469362+adit301104@users.noreply.github.com>
2025-03-28 14:47:57 +0530

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
               <span className="truncate">My Data</span>
             </button>
             
-            <Link to="/login" className="block w-full">
+            <Link to="/" className="block w-full">
               <button
                 onClick={() => setActiveButton("Logout")}
                 className={`flex items-center w-full text-base md:text-lg font-medium py-2 md:py-3 px-3 md:px-4 rounded transition-colors ${
